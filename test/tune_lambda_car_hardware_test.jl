04effc80b1a10e6c571288c80c3bd77e4de1a63c
mis0butter <junetter@gmail.com>
2023-09-15 19:25:53 -0500

    committing lambda car hardware test

--- test/tune_lambda_car_hardware_test.jl
+++ test/tune_lambda_car_hardware_test.jl
@@ -6,7 +6,8 @@ using LinearAlgebra
 
 path          = "test/data/jake_car_csvs/" 
 csv_files_vec = readdir( path ) 
-for i in eachindex(csv_files_vec)  
+# for i in eachindex(csv_files_vec)  
+for i = [1, 2]
     csv_files_vec[i] = string( path, csv_files_vec[i] ) 
 end
 
